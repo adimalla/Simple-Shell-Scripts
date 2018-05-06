3b8a4d4834eda7476ec2dda0f72098b6c74b260a
Aditya Mall <aditya.mall1990@gmail.com>
2018-05-06 16:17:29 +0530

    Some minor formatting done

--- refresh.sh
+++ refresh.sh
@@ -4,24 +4,20 @@
 ## Copyright: (c) 2018 by Aditya Mall.
 ## Email: <aditya.mall1990@gmail.com>
 
-
 ## Feel free to copy and distribute and don't forget to drop me a mail,          
 ## would love to hear from you regarding your views or if you have any questions. 
-
 
 ## General Description:-
 # Use this script in the present working diretory of xfce build to start over
 ##
 
-
 ## Usage:-
 # execute ./refresh.sh
 ##
 
-
 DIR_packs="Tar.Packages"
 DIR_unpacks="Build.Directories"
-TARGET_INSTALL="$(pwd)/XFCE_GUI/usr"                                            #User defined PATH, NOTE: Don't terminate with '/'
+TARGET_INSTALL="$(pwd)/XFCE_GUI/usr"                                 #User defined PATH, NOTE: Don't terminate with '/'
 TARGET_DIR=${TARGET_INSTALL%/*}
 
 
